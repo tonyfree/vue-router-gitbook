958122f357b6cbbeb2e7244dcd8129b52e654270
tony <fangtao_2008@163.com>
2017-09-03 18:39:05 +0800

    prepare

--- src/pages/component/component.js
+++ src/pages/component/component.js
@@ -1,66 +1,22 @@
-import Vue from 'vue'
 import 'css/style.css'
 import 'css/website.css'
 import 'css/vue.css'
 
+import Vue from 'vue'
 import routes from 'js/routerConfig.js'
 
-let components = {}
-routes.forEach(route => {
-  if (route.name) {
-    components[route.name] = require(`components/${route.name}.vue`)
-  }
-  if (route.children) {
-    route.children.forEach(child => {
-      if (child.name) {
-        components[child.name] = require(`components/${child.name}.vue`)
-      }
-    })
-  }
-})
-
-Vue.config.productionTip = false
 
 new Vue({
   el: '.book',
   data: {
-    routes,
-    currentView: '',
-    curPath: ''
+  
   },
   created() {
-    let path = location.hash.substr(1)
-
-    for (let i = 0, len = routes.length; i < len; i++) {
-      let cur = routes[i]
-      if (cur.path == path) {
-        this.currentView = cur.name
-        this.curPath = cur.path
-        return
-      }
-      if (cur.children) {
-        for (let i = 0, len = cur.children.length; i < len; i++) {
-          let sub = cur.children[i]
-          if (sub.path == path) {
-            this.currentView = sub.name
-            this.curPath = sub.path
-            return
-          }
-        }
-      }
-    }
+    
   },
   methods: {
-    to(route) {
-      if (route.path==undefined) return
-      // history.pushState({}, '', `${location.origin}/${route.path}`)
-      location.href = `#${route.path}`
-      this.currentView = route.name
-      this.curPath = route.path
-      document.title = `${route.title} · GitBook`
-    }
-  },
-  components
+    
+  }
 })
 
 
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,22 +3,13 @@ import 'css/website.css'
 import 'css/vue.css'
 
 import Vue from 'vue'
-import {router,routes} from './router'
 
 new Vue({
   el: '.book',
   data: {
-    routes
+
   },
   methods: {
-    to(route,subRoute) {
-      if (route.path==undefined) return
-      if(subRoute) {
-        router.push({path: `${route.path}/${subRoute.path}`})
-      } else {
-        router.push({path: `${route.path}`})
-      }
-    }
-  },
-  router
+    
+  }
 })

8e5bc3190c14e7e540e4f28a18d29d40be7ea514
tony <fangtao_2008@163.com>
2017-09-03 20:07:32 +0800

    add transition

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,7 @@
 import 'css/style.css'
 import 'css/website.css'
 import 'css/vue.css'
-import 'css/website.css'
+import './index.css'
 
 import Vue from 'vue'
 import {router,routes} from './router'
@@ -12,13 +12,11 @@ new Vue({
     routes
   },
   methods: {
-    to(route,subRoute) {
-      if (route.path==undefined) return
-      if(subRoute) {
-        router.push({path: `${route.path}/${subRoute.path}`})
-      } else {
-        router.push({path: `${route.path}`})
-      }
+    next() {
+
+    },
+    prev() {
+
     }
   },
   router
